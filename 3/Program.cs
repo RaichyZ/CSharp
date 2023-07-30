bdc3520d223f0a3c9c2a0ee1dba96ac6d27b0502
Ilya <original_find@mail.ru>
2023-07-30 15:10:39 +1200

    Dobavil 7dz

--- 3/Program.cs
+++ 3/Program.cs
@@ -53,13 +53,13 @@
 
 
 
-Задача 23
-Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-Console.WriteLine("Введите число: ");
-int num=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int num=Convert.ToInt32(Console.ReadLine());
 
-for (int i=1;i<=num;i++)
-{
-   Console.Write(Math.Pow(i,3)+" ");
-}
+// for (int i=1;i<=num;i++)
+// {
+//    Console.Write(Math.Pow(i,3)+" ");
+// }
---
+++ 7/Program.cs
@@ -0,0 +1,94 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// Console.Write("Введите количество строк массива: ");
+// int str= Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int stolb= Convert.ToInt32(Console.ReadLine());
+
+// double [,] matrix = new double[str,stolb];
+
+// for(int i = 0; i<matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j<matrix.GetLength(1); j++)
+//     {
+//         matrix[i,j]=new Random().NextDouble();
+//     }
+//     Console.WriteLine();
+// }
+
+// for(int i = 0; i<matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j<matrix.GetLength(1); j++)
+//     {
+//         Console.Write(matrix[i,j]+" ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// int [,] matrix = new int[5,5];
+
+// for(int i = 0; i<matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j<matrix.GetLength(1); j++)
+//     {
+//         matrix[i,j]=new Random().Next(0,10);
+//     }
+//     Console.WriteLine();
+// }
+
+// for(int i = 0; i<matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j<matrix.GetLength(1); j++)
+//     {
+//        Console.Write(matrix[i,j]+" ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine("Введите строку и столбец элемента, начиная нумерацию с 0: ");
+// int n=Convert.ToInt32(Console.ReadLine());
+// int m=Convert.ToInt32(Console.ReadLine());
+// if (n>=matrix.GetLength(0) || m>=matrix.GetLength(1))
+// {
+//     Console.WriteLine("Элемент вне диапозона массива.");
+// }
+// else Console.WriteLine(matrix[n,m]);
+
+
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+//в каждом столбце.
+
+// int [,] matrix = new int[5,5];
+// int sa=0;
+
+// for(int i = 0; i<matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j<matrix.GetLength(1); j++)
+//     {
+//         matrix[i,j]=new Random().Next(0,10);
+//     }
+//     Console.WriteLine();
+// }
+
+// for(int i = 0; i<matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j<matrix.GetLength(1); j++)
+//     {
+//        Console.Write(matrix[i,j]+" ");
+//     }
+//     Console.WriteLine();
+// }
+// for(int i = 0; i<matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j<matrix.GetLength(1); j++)
+//     {
+//        sa=sa+matrix[j,i];
+//     }
+//     Console.WriteLine($"Сумма {i} столбца: {sa}");
+//     sa=0;
+// }
